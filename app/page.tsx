ca8a72765e49b77747af419e3910617c622bd7f3
himeshsewa <himesh.panchal89@gmail.com>
2025-04-08 17:38:35 +0530

    setup nextjs project with shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
@@ -39,16 +40,17 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Login via GitHub
           </a>
-          <a
+          <Button>Click me</Button>
+          {/* <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             Read our docs
-          </a>
+          </a> */}
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
@@ -95,7 +97,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Build using NextJS, Supabase, Upstash and Hosted on Vercel.
         </a>
       </footer>
     </div>
